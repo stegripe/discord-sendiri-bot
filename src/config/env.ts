fda0aa459e98bda92035eab9461e4bb75c1aa605
mzrtamp <mozartrafi@stegripe.org>
2024-09-27 11:15:44 +0700

    chore(config/env): remove notifier channel since it no longer necessary

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,4 +4,3 @@ import { defaultPrefix } from "./constants.js";
 export const enableSlashCommand = process.env.ENABLE_SLASH_COMMAND !== "no";
 export const isDev = process.env.NODE_ENV === "development";
 export const prefix = isDev ? "dm!" : defaultPrefix;
-export const notifyChannel = process.env.NOTIFY_CHANNEL_ID;
