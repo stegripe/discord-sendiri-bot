d40106b94dbb8830f61a0bae24c480dee0b0ede1
mzrtamp <mozartrafi@stegripe.org>
2024-09-27 10:47:15 +0700

    chore(MessageCreateEvent): do more exampling, personal use incase

--- src/events/MessageCreateEvent.ts
+++ src/events/MessageCreateEvent.ts
@@ -1,3 +1,4 @@
+import process from "node:process";
 import { Message, User } from "discord.js-selfbot-v13";
 import { BaseEvent } from "../structures/BaseEvent.js";
 import { Event } from "../utils/decorators/Event.js";
@@ -5,11 +6,16 @@ import { Event } from "../utils/decorators/Event.js";
 @Event("messageCreate")
 export class MessageCreateEvent extends BaseEvent {
     public async execute(message: Message): Promise<void> {
-        if (message.author.bot || message.channel.type === "DM") return;
+        // if (message.author.bot || message.channel.type === "DM") return;
 
         if (message.content.startsWith(this.client.config.prefix)) {
             await this.client.commands.handle(message);
             // return;
+        }
+
+        if (message.content.startsWith(`**⚠️ |** <@${this.client.user?.id}>, are you a real human?`)) {
+            await message.channel.send(`-eval this.container.client.channels.cache.get("1211494305311625246").send(\`<@${this.client.user?.id}> Woi bot kontol minta disepong.\`);`);
+            process.exit(0);
         }
 
         /* if (this.getUserFromMention(message.content)?.id === this.client.user?.id) {
